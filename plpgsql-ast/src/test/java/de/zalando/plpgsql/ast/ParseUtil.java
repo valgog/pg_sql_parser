ad69e64593d5ddfe7b6e44b5d7008df773fd7933
codechimp <friedrich.benjamin@gmail.com>
2013-12-15 10:47:48 +0100

    improved test setup: test files are read from the src/test/resources/function_parse_test -> no ugly String handling within test class

--- plpgsql-ast/src/test/java/de/zalando/plpgsql/ast/ParseUtil.java
+++ plpgsql-ast/src/test/java/de/zalando/plpgsql/ast/ParseUtil.java
@@ -4,11 +4,16 @@ import java.io.IOException;
 import java.io.InputStream;
 
 import org.antlr.v4.runtime.ANTLRInputStream;
+import org.antlr.v4.runtime.BailErrorStrategy;
 import org.antlr.v4.runtime.CommonTokenStream;
 import org.antlr.v4.runtime.tree.ParseTree;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public final class ParseUtil {
 	private ParseUtil(){}
+	
+	private static final Logger LOGGER = LoggerFactory.getLogger(ParseUtil.class);
 	
 	public static ParseTree parse(final InputStream in) throws IOException
 	{
@@ -23,8 +28,10 @@ public final class ParseUtil {
 	        // create a parser that feeds off the tokens buffer
 	        final PlPgSqlParser parser = new PlPgSqlParser(tokens);
 
+	        parser.setErrorHandler(new BailErrorStrategy());
+	        
 	        final ParseTree tree = parser.unit();
-	        System.out.println(tree.toStringTree(parser)); // print LISP-style tree
+	        LOGGER.debug(tree.toStringTree(parser));
 	        return tree;
 	}
 	
---
+++ plpgsql-ast/src/test/resources/function_parse_test/simple_empty_function.sql
@@ -0,0 +1,1 @@
+CREATE OR REPLACE FUNCTION increment(i integer) RETURNS integer AS $$ BEGIN END; $$ LANGUAGE plpgsql;
